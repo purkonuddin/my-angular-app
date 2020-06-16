79c21f6e6c5311f3bd87dad2bce7aa0a61261fa5
purkonuddin <44079569+purkonuddin@users.noreply.github.com>
2020-06-17 01:44:59 +0700

    landing page
    fetch API for weather and news

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WeatherModule } from './weather/weather.module';
+import { NotificationsModule } from './notifications/notifications.module';
+import { NewsApiModule } from './news-api/news-api.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    WeatherModule,
+    NotificationsModule,
+    NewsApiModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/apps/weather/forecast/forecast.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { ForecastService } from '../forecast.service';
+
+@Component({
+  selector: 'app-forecast',
+  templateUrl: './forecast.component.html',
+  styleUrls: ['./forecast.component.css']
+})
+export class ForecastComponent implements OnInit {
+
+  constructor(forecastService: ForecastService){
+    forecastService.getCurrentLocation()
+    .subscribe((coords) => {
+      console.log( coords );
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+}
